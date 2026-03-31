62baf3204912527e01c1d77ef5c5769c5f5ce942
Sanaa Loutfi <sl@MacBook-Pro.local>
2026-03-31 23:44:06 +0200

    Hardcode Supabase config

--- src/supabase.js
+++ src/supabase.js
@@ -1,12 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL
-const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: true
-  }
-})
+export const supabase = createClient(
+  'https://xnrcognyudbubtpfrga.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhucmNvZ255eXVkYnVidHBmcmdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4OTg3MTksImV4cCI6MjA5MDQ3NDcxOX0.GGL9fnO0zbGJ6j0_If4sJmr6lfIktDc8QTClrgNbz0Y',
+  { auth: { autoRefreshToken: true, persistSession: true, detectSessionInUrl: true } }
+)
