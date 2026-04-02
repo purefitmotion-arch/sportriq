d39dac0178ccea7c26360d2414da0dd0c5582f97
Sanaa Loutfi <sl@MacBook-Pro.local>
2026-04-02 22:28:58 +0200

    Fix Supabase URL double Y

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://xnrcognyudbubtpfrga.supabase.co',
+  'https://xnrcognyyudbubtpfrga.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhucmNvZ255eXVkYnVidHBmcmdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4OTg3MTksImV4cCI6MjA5MDQ3NDcxOX0.GGL9fnO0zbGJ6j0_If4sJmr6lfIktDc8QTClrgNbz0Y',
   { auth: { autoRefreshToken: true, persistSession: true, detectSessionInUrl: true } }
 )
